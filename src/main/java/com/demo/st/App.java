c9498e725f628a82586fe7c2bcd6d3dd8b149433
Chakraborty <lopamudra.chakraborty@capgemini.com>
2021-01-04 20:49:59 +0530

    case 1,2,3,5,6

--- src/main/java/com/demo/st/App.java
+++ src/main/java/com/demo/st/App.java
@@ -1,7 +1,9 @@
 package com.demo.st;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.demo.st.exception.STExceptions;
 
@@ -13,12 +15,27 @@ public class App
 {
    public int add(String number) throws STExceptions {
 	   int sum=0;
+	   List<Integer> negative= new ArrayList<Integer>();
 	   if(!number.trim().isEmpty()) {
 		try {
-			int[] numArray = Arrays.stream(number.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
+			int[] numArray = Arrays.stream(number.split(",|\n|;")).map(String::trim).mapToInt(Integer::parseInt).toArray();
 			
 			for (int i = 0; i < numArray.length; i++) {
-				sum=sum+numArray[i];
+				if(numArray[i]<0) {
+					negative.add(numArray[i]);
+					sum=0;
+				}
+				else if(numArray[i]>=1000) {
+					numArray[i]=0;
+				}
+				else {
+					sum=sum+numArray[i];
+				}
+				
+			}
+			if(negative.size()>0) {
+				sum=0;
+				throw new STExceptions("Negative num not allowed! Please correct the following "+negative);
 			}
 	   }
 		catch(Exception e) {
--- src/test/java/com/demo/st/AppTest.java
+++ src/test/java/com/demo/st/AppTest.java
@@ -2,6 +2,7 @@ package com.demo.st;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.demo.st.exception.STExceptions;
@@ -13,17 +14,56 @@ import com.demo.st.exception.STExceptions;
 public class AppTest 
 {
     App app= new  App();
+    
     @Test
-    public void testFindMax(){  
+    public void case0(){  
+    	System.out.println("Initial test case started");
+    	try {
+			assertEquals(0,app.add(""));
+			assertEquals(10,app.add("1,2,3,4")); //Case2
+		} catch (STExceptions e) {
+			System.out.println("Exception is: "+e.getMessage());
+		}
+    	System.out.println("Initial test case completed");  
+       
+    }
+    @Test
+    public void case3(){  
     	try {
 			
-			assertEquals(0,app.add(""));
-			assertEquals(10,app.add("1,2,3,4"));
-			assertEquals(0,app.add("1,2,cd,e"));
+    		System.out.println("Test case3 started");
+			assertEquals(3,app.add("1\n2")); //Case3
+			//assertEquals(4,app.add("1,\n3")); //Case3 --check
+			assertEquals(STExceptions.class,app.add("1,2,cd,e"));
 		} catch (STExceptions e) {
 			System.out.println("Exception is: "+e.getMessage());
 		}
          
-       
+    	System.out.println("Test case3 completed");
+    }
+    @Test
+    public void case5(){  
+    	System.out.println("Test case5 started");
+    	try {
+			
+			//assertEquals(3,app.add("1\n2;-8")); //Case5
+			assertEquals(STExceptions.class,app.add("1\n2;-8,-2")); //Case5
+		} catch (STExceptions e) {
+			System.out.println("Exception is: "+e.getMessage());
+		}
+         
+    	System.out.println("Test case5 completed");
+    }
+    @Test
+    public void case6(){  
+    	System.out.println("Test case6 started");
+    	try {
+			assertEquals(1002,app.add("1\n2;999,1000,1001")); //Case6
+			assertEquals(3,app.add("1\n2;1001")); //Case6
+		} catch (STExceptions e) {
+			System.out.println("Exception is: "+e.getMessage());
+		}
+         
+    	System.out.println("Test case6 completed");
     }
 }
